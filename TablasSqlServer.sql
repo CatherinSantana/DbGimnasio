1b9dcd912c6ebf612a7c5fdd8dcf8aaec8789efe
CatherinSantana <69719391+CatherinSantana@users.noreply.github.com>
2020-08-19 11:03:12 -0500

    Add files via upload

--- TablasSqlServer.sql
+++ TablasSqlServer.sql
@@ -1,28 +1,63 @@
-insert into Clientes values
-(1,'Daniela','Camacaro','F','daniela@hotmail.com','Manta','0987568957'),
-(2,'Luis','Mendoza','M','luis23@hotmail.com','Montecristi','0990876432'),
-(3,'Sinai','Roca','F','sinai14@hotmail.com','Portoviejo','0979868957'),
-(4,'Miguel','Delgado','M','miguel@hotmail.com','Colorado','0987568653'),
-(5,'Ranyelis','Mora','F','ranyelis@hotmail.com','Las Cumbres','0967868957');
+
+/*========================================================*/
+/* Table: CLIENTE                                         */
+/*========================================================*/
+
+create table Clientes (
+Id_cliente int not null,
+Nombre varchar (50),
+Apellido varchar(50),
+Genero varchar (10),
+Correo varchar (30),
+Direccion varchar (100),
+Telefono varchar (10),
+constraint pk_Id_cliente primary key (Id_cliente)
+)
+go
+
+/*==============================================================*/
+/* Table: INSTRUCTOR                                            */
+/*==============================================================*/
+
+create table Instructor(
+Id_instructor int not null,
+Nombre varchar (50),
+Apellido varchar(50),
+Genero varchar (10),
+Correo varchar (30),
+Direccion varchar (100),
+Telefono varchar (10),
+Especialidad varchar (50),
+constraint pk_Id_instructor primary key (Id_instructor)
+) 
+go
+
+/*==============================================================*/
+/* Table: GRUPO                                                 */
+/*==============================================================*/
+
+ create table Grupo(
+Id_grupo int not null,
+Id_instructor int,
+grupo varchar (50),
+Horario varchar(50),
+fecha_inicio date,
+fecha_final date,
+constraint pk_Id_grupo primary key (Id_grupo,Id_instructor)
+)
+go
 
 
- insert into Instructor values
-(1,'Giselle','Avila','F','avila@hotmail.com','Manta','0987568957','Pilates'),
-(2,'Ricardo','Zambrano','M','ricardoz@hotmail.com','San pedro','0990876432','Boxeo'),
-(3,'Roberto','Gil','M','gil23hotmail.com','Los Esteros','0979868957','Aeròbicos' ),
-(4,'Carol','Mejias','F','g4523hotmail.com','Los Esteros','0979868957', 'Spinning');
+/*==============================================================*/
+/* Table: INSCRIPCION                                           */
+/*==============================================================*/
 
-
-insert into Grupo values
-(1,'1','Pilates','Martes 14:00','24-07-2020','30-08-2020'),
-(2,'3','Aeròbicos','Lunes 09:00','16-07-2020','24-09-2020'),
-(3,'2','Boxeo','Viernes 10:00','13-07-2020','24-09-2020'),
-(4,'5','Spinning','Jueves 08:00','16-07-2020','24-09-2020');
-
- insert into Inscripcion values
-(1,'1','1','08-07-2020','25.00'),
-(2,'2','2','10-07-2020','20.00'),
-(3,'4','2','06-07-2020','20.00'),
-(4,'5','2','09-07-2020','20.00'),
-(5,'3','3','09-07-2020','20.00');
-
+create table Inscripcion(
+Nro_inscripcion int not null,
+Id_cliente int,
+Id_grupo int,
+Fecha_inscripcion date,
+cuota float,
+constraint pk_Nro_inscripcion primary key (Nro_inscripcion,Id_cliente,Id_grupo)
+)
+go
